a2a8ee4d2dcf50f165e70057fc53ddf7ac1cbbbf
ٌٍRanaYahya <Ryalnajjar@gmail.com>
2022-06-28 11:29:36 +0300

    first commit

---
+++ app/src/main/java/com/seu/kfmctrainingapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.seu.kfmctrainingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
